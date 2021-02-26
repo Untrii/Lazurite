d4a6e7e920753dee47063e54e6d423a0610017f7
Azullon <snick159ers@gmail.com>
2021-02-26 19:01:04 +0300

    [add] Debuggin stuff

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import 'preact/debug'
 import { render, h } from 'preact'
 import App from './components/App'
+import * as dataLoader from './slideRenderer/dataLoader'
+
+window['dataLoader'] = dataLoader
 
 render(<App />, document.querySelector('#app'))
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+import Color from '@/models/common/Color'
 import TextSlideObject from '@/models/presentation/slideObjects/TextSlideObject'
 import AppStateModel from '@/models/store/AppStateModel'
 import randomString from '@/util/randomString'
@@ -6,7 +7,21 @@ import { reactive } from '@/util/reactivity'
 export const raw = new AppStateModel()
 
 raw.currentTab.openedPresentation.slides.push([])
-raw.currentTab.currentSlide.push(new TextSlideObject())
+let text = new TextSlideObject()
+text.content = 'Text block\nLine2'
+text.top = 270
+text.left = 480
+text.height = 540
+text.width = 960
+text.style.fontSize = 40
+text.verticalAlign = 'bottom'
+text.horizontalAlign = 'middle'
+raw.currentTab.currentSlide.push(text)
+raw.currentTab.openedPresentation.theme.background = {
+  medianColor: new Color(),
+  value: '#C7E8FF',
+  type: 'color',
+}
 
 const reactiveStore = reactive(raw)
 
