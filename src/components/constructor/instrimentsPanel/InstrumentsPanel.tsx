97da2f55b5f04980f36c6330ce5e33543e21c6bd
Azullon_notebook <snick159ers@gmail.com>
2021-03-01 15:47:36 +0300

    [add] Initial instruments panel

--- src/components/constructor/instrimentsPanel/InstrumentsPanel.tsx
+++ src/components/constructor/instrimentsPanel/InstrumentsPanel.tsx
@@ -1,12 +1,38 @@
 import './InstrumentsPanel.scss'
 import { h } from 'preact'
-import VerticalNav from '@/components/controls/VerticalNav'
+import VerticalNav, { INavItem } from '@/components/controls/VerticalNav'
+import assets from '@/assets'
+import { useState } from 'preact/hooks'
 
 const InstrumentsPanel = () => {
+  const tabs: INavItem[] = [
+    {
+      icon: assets.add,
+      name: 'add',
+      displayName: 'Add',
+    },
+    {
+      icon: assets.edit,
+      name: 'edit',
+      displayName: 'Edit',
+    },
+    {
+      icon: assets.history,
+      name: 'history',
+      displayName: 'History',
+    },
+  ]
+
+  const [tabIndex, setTabIndex] = useState(0)
+
+  const onChange = function (index: number) {
+    setTabIndex(index)
+  }
+
   return (
     <div class="instruments-panel">
       <div class="instruments-panel__content"></div>
-      <VerticalNav items={[]} />
+      <VerticalNav items={tabs} selectedItemIndex={tabIndex} onChange={onChange} />
     </div>
   )
 }
