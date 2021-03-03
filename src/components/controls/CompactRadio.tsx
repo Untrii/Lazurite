3eb61512fdbbb5c45ca2a5dadf52da16ff5f915f
Azullon_notebook <snick159ers@gmail.com>
2021-03-03 10:29:55 +0300

    [add] Compact radio

--- src/components/controls/CompactRadio.tsx
+++ src/components/controls/CompactRadio.tsx
@@ -1,12 +1,42 @@
-//import './CompactRadio.scss'
+import './CompactRadio.scss'
 import { h } from 'preact'
+import Prepend from './Prepend'
+
+export interface IRadioVariant {
+  displayName: string
+  icon: string
+}
 
 interface ICompactRadioProps {
   prepend?: string
-  variants
+  variants: IRadioVariant[]
+  selectedVariantIndex?: number
+  colorName?: string
+  onSelected?: (index: number) => void
 }
 
-const CompactRadio = (props: ICompactRadioProps) => {
-  return
+const CompactRadio = ({
+  prepend,
+  colorName = 'blue-400',
+  variants,
+  selectedVariantIndex = 0,
+  onSelected,
+}: ICompactRadioProps) => {
+  const renderVariant = function (variant: IRadioVariant, index: number) {
+    const variantClasses: string[] = ['compact-radio__variant', 'control-bg_' + colorName, 'border-dark_' + colorName]
+    if (index == selectedVariantIndex) variantClasses.push('pressed-control-bg_' + colorName)
+    return (
+      <div class={variantClasses.join(' ')} onClick={() => onSelected(index)}>
+        <img src={variant.icon} alt={variant.displayName} />
+      </div>
+    )
+  }
+
+  return (
+    <div class="compact-radio">
+      {prepend ? <Prepend>{prepend}</Prepend> : null}
+      <div class="compact-radio__variants">{variants.map((value, index) => renderVariant(value, index))}</div>
+    </div>
+  )
 }
 export default CompactRadio
