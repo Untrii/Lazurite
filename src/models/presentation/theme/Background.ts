9d082ded7698273026df82a5a685ea2199e5f957
Azullon <snick159ers@gmail.com>
2021-03-15 12:14:09 +0300

    [upd] updated background model

--- src/models/presentation/theme/Background.ts
+++ src/models/presentation/theme/Background.ts
@@ -1,9 +1,10 @@
 import Color from '@/models/common/Color'
 
-type BackgroundType = 'color' | 'gradient' | 'gradicolor' | 'pattern' | 'image'
+export type BackgroundType = 'color' | 'gradient' | 'gradicolor' | 'pattern' | 'image'
 
 export default class Background {
   medianColor = Color.white
   type = 'color' as BackgroundType
   value = Color.white.toHex()
+  displayValue = Color.white.toHex()
 }
