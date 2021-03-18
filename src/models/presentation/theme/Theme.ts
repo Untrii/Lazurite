03a2f7e6a9c3c2f57d694db21f83091657a9e072
Azullon <snick159ers@gmail.com>
2021-03-18 20:17:26 +0300

    [upd] replacing palette with defaults

--- src/models/presentation/theme/Theme.ts
+++ src/models/presentation/theme/Theme.ts
@@ -5,5 +5,13 @@ import FontPreset from './FontPreset'
 export default class Theme {
   background = new Background()
   fontPresets = [] as FontPreset[]
-  palette = [] as Color[]
+  defaults = {
+    tableRowBG: Color.white,
+    tableRowStrippedBG: Color.white,
+    tableHeaderBG: Color.white,
+
+    mainText: Color.black,
+    accentText0: Color.black,
+    accentText1: Color.black,
+  }
 }
