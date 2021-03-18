b395a6598e799863b190b176622e106a1265f919
Azullon <snick159ers@gmail.com>
2021-03-18 20:16:08 +0300

    [add] color manipulation actions

--- src/store/actions/design.ts
+++ src/store/actions/design.ts
@@ -1,1 +1,31 @@
+import Background from '@/models/presentation/theme/Background'
 import store from '@/store'
+import io from '@/io'
+import Color from '@/models/common/Color'
+import { saveCurrentPresentation } from './util'
+
+type DefaultsName = keyof typeof store.currentTab.openedPresentation.theme.defaults
+
+const getCurrentTheme = function () {
+  return store.currentTab.openedPresentation.theme
+}
+
+export function addUserBackground(bg: Background) {
+  store.userBackgrounds[bg.type] = store.userBackgrounds[bg.type].filter((item) => item.value != bg.value)
+  store.userBackgrounds[bg.type].push(bg)
+  io.saveUserBackgrounds(store.userBackgrounds)
+}
+
+export function changeDefaultColor(defaultName: DefaultsName, color: Color) {
+  const theme = getCurrentTheme()
+  theme.defaults[defaultName] = color
+  saveCurrentPresentation()
+}
+
+export function changeBackground(bg: Background) {
+  const theme = getCurrentTheme()
+  theme.background = bg
+  saveCurrentPresentation()
+}
+
+window['addUserBackground'] = addUserBackground
