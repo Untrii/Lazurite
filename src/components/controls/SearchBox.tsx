2be92cef39aca9cbc51c7074508b9cf8e8aaaa58
Azullon_notebook <snick159ers@gmail.com>
2021-03-24 21:50:37 +0300

    [add] maxLength property in searchbox

--- src/components/controls/SearchBox.tsx
+++ src/components/controls/SearchBox.tsx
@@ -5,15 +5,22 @@ interface ISearchBoxProps {
   onInput?: (value: string) => void
   colorName?: string
   placeholder?: string
+  maxLength?: number
 }
 
-const SearchBox = ({ onInput, colorName = 'blue-300', placeholder = 'Input text...' }: ISearchBoxProps) => {
+const SearchBox = ({
+  onInput,
+  colorName = 'blue-300',
+  placeholder = 'Input text...',
+  maxLength = 40,
+}: ISearchBoxProps) => {
   return (
     <input
       type="text"
       class={'search-box bg_' + colorName}
       placeholder={placeholder}
       onInput={(event) => onInput?.((event.target as any).value)}
+      maxLength={maxLength}
     />
   )
 }
