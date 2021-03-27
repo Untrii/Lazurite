28fe59a18465be562e179a74e17010b8670e82e4
Azullon_notebook <snick159ers@gmail.com>
2021-03-27 12:15:16 +0300

    [upd] rgb() values support

--- src/util/getMedianColorSync.ts
+++ src/util/getMedianColorSync.ts
@@ -1,4 +1,5 @@
 import Color from '@/models/common/Color'
+import parseGradient from './parseGradient'
 
 export default function getMedianColorSync(type: 'color' | 'gradient' | 'gradicolor', value: string) {
   switch (type) {
@@ -6,15 +7,20 @@ export default function getMedianColorSync(type: 'color' | 'gradient' | 'gradico
       return Color.fromHex(value)
     case 'gradicolor':
     case 'gradient':
-      let cols = value.split(' ')
+      const { stops } = parseGradient(value)
 
-      let col0 = Color.fromHex(cols[1])
-      let col1 = Color.fromHex(cols[3])
+      let r = 0,
+        g = 0,
+        b = 0
+      for (let i = 1; i < stops.length; i++) {
+        const range = stops[i].percent - stops[i - 1].percent
+        const color = stops[i].color
+        const prevColor = stops[i - 1].color
+        r += ((prevColor.r + color.r) / 2) * range
+        g += ((prevColor.g + color.g) / 2) * range
+        b += ((prevColor.b + color.b) / 2) * range
+      }
 
-      return Color.fromRgb(
-        Math.floor((col0.r + col1.r) / 2),
-        Math.floor((col0.g + col1.g) / 2),
-        Math.floor((col0.b + col1.b) / 2)
-      )
+      return Color.fromRgb(Math.floor(r), Math.floor(g), Math.floor(b))
   }
 }
