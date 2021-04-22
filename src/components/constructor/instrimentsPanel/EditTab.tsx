f1da4f28cc45aa0c821f59fec6f7ce1b63cbaaf3
Azullon_notebook <snick159ers@gmail.com>
2021-04-22 15:58:54 +0300

    [add] selected items count

--- src/components/constructor/instrimentsPanel/EditTab.tsx
+++ src/components/constructor/instrimentsPanel/EditTab.tsx
@@ -1,12 +1,26 @@
-//import './EditTab.scss'
+import './EditTab.scss'
 import { h, JSX } from 'preact'
 import { raw as store } from '@/store'
 import PositionEditor from './editors/PositionEditor'
+import useEventBus from '@/store/useEventBus'
 
 const EditTab = () => {
+  useEventBus(store, 'slideChange', store.getCurrentSlide())
   const editors: JSX.Element[] = []
   editors.push(<PositionEditor />)
 
-  return <div class="edit-tab">{editors}</div>
+  let text = 'nothing'
+  const selection = store.currentTab.selection
+  if (!selection.isEmpty) {
+    if (selection.size == 1) text = selection.items[0].type
+    else text = `${selection.size} objects`
+  }
+
+  return (
+    <div class="edit-tab">
+      <h2>Selected: {text}</h2>
+      {editors}
+    </div>
+  )
 }
 export default EditTab
